52badecb773da33fef57022dd98754633e1325ea
martuanali318-cell <martuanali318@gmail.com>
2025-11-22 18:41:36 -0300

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,47 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img-20251023-wa0016",
+      "name": "IMG-20251023-WA0016",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.6886288718680067,
+          "pitch": 0.08526440205541164,
+          "title": "Obra de Vilamajo&nbsp;",
+          "text": "PEGAR LINKKK"
+        },
+        {
+          "yaw": -0.5800374989829571,
+          "pitch": 0.1508113241211646,
+          "title": "Habitacion de Estrella&nbsp;",
+          "text": "Pensada inicialmente como un vestidor para Mercedes. Con la llegada de Estrella a la casa se convierte en su habitación, separada del corredor por una cortina.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
